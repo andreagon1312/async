e755d68fe09891837ca37725ec2efed538f2f031
andreagon1312 <145371737+andreagon1312@users.noreply.github.com>
2023-11-14 11:58:55 -0400

    Add files via upload

--- asincronisno/js/dex.js
+++ asincronisno/js/dex.js
@@ -1,12 +1,15 @@
 const paragraph = document.getElementById('paragraph');
 const loadP = document.getElementById('loadP');
 const loader = document.getElementsByClassName('lds-roller')
+const inputCity = document.getElementById('inputCity');
+const btnSearch = document.getElementById('btnSearch');
+const nameCountry = document.getElementById('nameCountry');
+const city = document.getElementById('city');
+const temp = document.getElementById('temp');
+const wind = document.getElementById('wind');
 const APIKEY = '6637faa8d9e38591343e0d4ba75bd213';
-/*setTimeout(() => {
-  loadP.classList.add('show');
-  loader[0].style.display = 'none'
 
-}, 3000)*/
+
 
 const openWeather = async (city) => {
     const getDataFromAPIWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`)
@@ -15,9 +18,30 @@ const openWeather = async (city) => {
 
 }
 
-openWeather('London')
+btnSearch.addEventListener('click', () => {
+    loader[0].classList.add('show')
+    
+    openWeather(inputCity.value)
     .then((respuestaDelClima) => {
-        console.log(respuestaDelClima)
-})
+        
+        loader[0].classList.remove('show');
+        loader[0].classList.add('none');
+        console.log(respuestaDelClima);
+        nameCountry.innerText = respuestaDelClima.sys.country
+        city.innerText = respuestaDelClima.name
+        temp.innerText = (respuestaDelClima.main.temp - 273.15).toFixed(2)
+        wind.innerText = respuestaDelClima.wind.speed
+
+    })
     .catch((error) => console.log(error));
+} )
+
+
+
+
+
+
+
+
+
 
